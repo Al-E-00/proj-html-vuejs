74e23b0b1966b2e17f90123a2ca222d5d8aa3aab
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-05 16:30:33 +0200

    Scroll animation improved

--- src/components/GetInTouchSection.vue
+++ src/components/GetInTouchSection.vue
@@ -8,30 +8,30 @@
                         <h3 data-aos="fade-up" data-aos-delay="200">Get in <span class="title-stylized">Touch</span></h3>
                         <span data-aos="fade-up" data-aos-delay="400" class="text-get-in-touch">We will respond to your message as soon as possible.</span>
 
-                        <div data-aos="fade-left" class="row row-cols-2 g-3 input-two-cols-container">
+                        <div class="row row-cols-2 g-3 input-two-cols-container">
                             <div class="col">
-                                <div class="form-floating">
+                                <div class="form-floating" data-aos="fade-up" data-aos-delay="850">
                                     <input type="text" class="form-control" id="floatingInput" placeholder="Name">
                                     <label for="floatingInput">Name</label>
                                 </div>
                             </div>
 
                             <div class="col">
-                                <div class="form-floating">
+                                <div class="form-floating" data-aos="fade-up" data-aos-delay="850">
                                     <input type="email" class="form-control" id="floatingEmail" placeholder="Email">
                                     <label for="floatingPassword">Email</label>
                                 </div>
                             </div>
 
                             <div class="col">
-                                <div class="form-floating">
+                                <div class="form-floating" data-aos="fade-up" data-aos-delay="600">
                                     <input type="text" class="form-control" id="floatingInput" placeholder="Phone">
                                     <label for="floatingInput">Phone</label>
                                 </div>
                             </div>
 
                             <div class="col">
-                                <div class="form">
+                                <div class="form" data-aos="fade-up" data-aos-delay="600">
                                     <select class="form-select p-3" id="floatingSelect"
                                         aria-label="Floating label select">
                                         <option selected>More info</option>
@@ -43,12 +43,12 @@
                                 </div>
                             </div>
                         </div>
-                        <div  data-aos="fade-left" class="form pt-3">
+                        <div  class="form pt-3" data-aos="fade-up" data-aos-delay="400">
                             <textarea class="form-control" placeholder="Message" id="floatingTextarea2"
                                 style="height: 150px"></textarea>
 
                         </div>
-                        <button  data-aos="fade-left" class="btn btn-primary">Send</button>
+                        <button  data-aos="fade-up" data-aos-delay="400" class="btn btn-primary">Send</button>
                     </div>
                     <div  data-aos="fade-left" data-aos-delay="100" class="col right-part-get-in-touch">
                         <h6>Example Inc.</h6>
